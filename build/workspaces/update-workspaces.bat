753ed0a6380b591f4faa779c909bd4f961907845
Shafty89 <oula_89@hotmail.com>
2020-04-25 12:49:08 +0300

    wxWidgets library

--- build/workspaces/update-workspaces.bat
+++ build/workspaces/update-workspaces.bat
@@ -2,5 +2,6 @@
 rem ** Create Visual Studio Workspaces on Windows **
 
 cd ..\premake
+--atlas
 if not exist ..\workspaces\vc2015\SKIP_PREMAKE_HERE premake5\bin\release\premake5 --outpath="../workspaces/vc2015" --use-shared-glooxwrapper %* vs2015
 cd ..\workspaces
