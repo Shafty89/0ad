acd78a9f87061d5de5f9b3a20555539158549455
Hassan Shahbazi <h-shahbazi@hotmail.com>
2020-04-26 17:10:21 +0300

    resolve github action errors

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -6,15 +6,20 @@ export SHELL=/bin/bash
 # install dependencies
 sudo ./ubuntu-dependencies.sh
 
+# local variable (number of CPU cores)
+n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
+
 # build workspace
+set -e
+svn up
 cd build/workspaces/
 ./clean-workspaces.sh
-./update-workspaces.sh --with-system-nvtt --with-system-mozjs45 -j5
+./update-workspaces.sh --with-system-nvtt --with-system-mozjs45 -j $n
 
 # build tests
 cd gcc
 make clean
-make test -j5
+make -j $n
 
 # run tests
 cd ../../../binaries/system
--- ubuntu-dependencies.sh
+++ ubuntu-dependencies.sh
@@ -7,7 +7,7 @@ apt-add-repository ppa:boost-latest/ppa
 add-apt-repository ppa:mhier/libboost-latest
 apt update --allow-insecure-repositories
 
-apt install libopencv-dev libboost-dev libboost-all-dev libboost-filesystem-dev libboost-python-dev libboost1.68-dev libboost1.68 python-dev build-essential cmake gcc python libcurl4-openssl-dev libenet-dev libgloox-dev libicu-dev libogg-dev libpng-dev libsodium-dev libvorbis-dev libxcursor-dev libxml2-dev miniupnpc libopenal-dev freeglut3-dev libsdl2-dev libpng-dev libwebkitgtk libminiupnpc-dev g++ libnvtt-dev subversion zlib1g-dev libboost-system-dev libmozjs-45-dev
+apt-get install build-essential cmake libboost-dev libboost-filesystem-dev libcurl4-gnutls-dev libenet-dev libgloox-dev libicu-dev libminiupnpc-dev libnvtt-dev libogg-dev libopenal-dev libpng-dev libsdl2-dev libvorbis-dev libxcursor-dev libxml2-dev python subversion zlib1g-dev libenet-dev libboost-system-dev
 apt install libwebkit --allow-unauthenticated 
 apt install wx3.0-headers=3.0.4+dfsg-8 libwxbase3.0-dev=3.0.4+dfsg-8 libwxgtk3.0-dev=3.0.4+dfsg-8 libwxbase3.0-0v5=3.0.4+dfsg-8 libwxgtk3.0-0v5=3.0.4+dfsg-8s --allow-unauthenticated
 apt policy wx3.0-headers libwxgtk3.0-dev --allow-unauthenticated
