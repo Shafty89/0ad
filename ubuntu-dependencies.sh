3f66a6e6c24989ce77d41646790d55339404b3f9
Hassan Shahbazi <h-shahbazi@hotmail.com>
2020-04-26 14:59:32 +0300

    trying to resolve the issue, AGAIN

--- ubuntu-dependencies.sh
+++ ubuntu-dependencies.sh
@@ -4,9 +4,10 @@
 apt-add-repository 'deb http://repos.codelite.org/wx3.1.0/ubuntu/ artful universe'
 apt-add-repository 'deb http://cz.archive.ubuntu.com/ubuntu bionic main universe'
 apt-add-repository ppa:boost-latest/ppa
+add-apt-repository ppa:mhier/libboost-latest
 apt update --allow-insecure-repositories
 
-apt install libopencv-dev libboost-dev libboost-all-dev libboost-filesystem-dev aptitude build-essential cmake gcc python2.7 libcurl4-openssl-dev libenet-dev libgloox-dev libicu-dev libogg-dev libpng-dev libsodium-dev libvorbis-dev libxcursor-dev libxml2-dev miniupnpc libopenal-dev freeglut3-dev libsdl2-dev libpng-dev libwebkitgtk libminiupnpc-dev g++
-apt install libwebkit
-apt policy wx3.0-headers libwxgtk3.0-dev
+apt install libopencv-dev libboost-dev libboost-all-dev libboost-filesystem-dev libboost-python-dev libboost1.68-dev libboost1.68 python-dev build-essential cmake gcc python2.7 libcurl4-openssl-dev libenet-dev libgloox-dev libicu-dev libogg-dev libpng-dev libsodium-dev libvorbis-dev libxcursor-dev libxml2-dev miniupnpc libopenal-dev freeglut3-dev libsdl2-dev libpng-dev libwebkitgtk libminiupnpc-dev g++
+apt install libwebkit --allow-unauthenticated
+apt policy wx3.0-headers libwxgtk3.0-dev --allow-unauthenticated
 apt-get install libwxbase3.1-0-unofficial libwxbase3.1-dev libwxgtk3.1-0-unofficial libwxgtk3.1-dev wx3.1-headers wx-common libwxgtk-webview3.1-0-unofficial libwxgtk-webview3.1-dev libwxgtk-media3.1-0-unofficial libwxgtk-media3.1-dev libwxbase3.1-0-unofficial-dbg libwxgtk-webview3.1-0-unofficial-dbg libwxgtk-media3.1-0-unofficial-dbg wx3.1-i18n wx3.1-examples --allow-unauthenticated
