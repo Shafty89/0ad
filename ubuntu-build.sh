1bacd588c84d7c60ff50b0136ba88c7e8611d630
Hassan Shahbazi <h-shahbazi@hotmail.com>
2020-04-26 13:48:39 +0300

    trying to resolve stupid issues with github action

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -3,15 +3,17 @@
 # fix the fucking error with stupid github action
 export SHELL=/bin/bash
 
+# install dependencies
+sudo ./ubuntu-dependencies.sh
+
 # build workspace
-set -e
 cd build/workspaces/
 ./clean-workspaces.sh
 ./update-workspaces.sh -j5
 
+# build tests
 cd gcc
 make clean
-make -j5
 make test -j5
 
 # run tests
