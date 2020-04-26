756a336fe886a6ab9df637686d8615e0f10006e9
Shafty89 <oula_89@hotmail.com>
2020-04-26 17:55:47 +0300

    Revert "update build param"
    This reverts commit 76546acdac524f4d34de42705c2b2a3fbdc6487d.

--- ubuntu-build.sh
+++ ubuntu-build.sh
@@ -13,7 +13,7 @@ n=`cat /proc/cpuinfo | grep "cpu cores" | uniq | awk '{print $NF}'`
 set -e
 cd build/workspaces/
 ./clean-workspaces.sh
-./update-workspaces.sh --with-system-nvtt --with-system-mozjs45 --disable-atlas -j $n
+./update-workspaces.sh --with-system-enet --with-system-nvtt --with-system-mozjs45 --disable-atlas -j $n
 
 # build tests
 cd gcc
